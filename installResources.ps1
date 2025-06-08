64b68c5189a3b7b6b410584d861e5afb2ba0392a
Mateusz <dsw55990@student.dsw.edu.pl>
2025-06-08 14:05:06 +0200

    zaktualizowanie skryptu z pluginami

--- installResources.ps1
+++ installResources.ps1
@@ -1,2 +1,8 @@
 # Python package installation script
 pip install --upgrade pip
+pip install PyQt5
+pip install pyyaml
+pip install xmltodict
+pip install pyyaml
+pip install xmltodict
+pip install PyQt5
